1608c49827bd9a32c89edb0c0aaa454385f4f716
Tobias Dammers <tdammers@gmail.com>
2023-04-16 23:32:37 +0200

    Things.

--- cli/Main.hs
+++ cli/Main.hs
@@ -4,18 +4,42 @@ module Main where
 
 import Database.Posterchild
 import Control.Monad (forM_)
+import qualified Data.Map as Map
 
 query :: SelectQuery
 query =
   SelectQuery
     { selectFrom = SelectFromTable "posts"
     , selectFields = SelectFields
-        [ SelectField (SelectColumn "posts" "id") Nothing
-        , SelectField (SelectColumn "posts" "title") Nothing
-        , SelectField (SelectColumn "posts" "body") Nothing
+        [ SelectField (SelectColumn (ColumnRef "posts" "id")) Nothing
+        , SelectField (SelectColumn (ColumnRef "posts" "title")) Nothing
+        , SelectField (SelectColumn (ColumnRef "posts" "body")) (Just "body")
+        , SelectField (SelectParam "foobar") Nothing
+        , SelectField (SelectValue $ SqlInt 23) (Just "twenty_three")
+        , SelectField (SelectSubquery $
+            SelectQuery
+              { selectFrom = SelectFromTable "users"
+              , selectFields = SelectFields
+                [ SelectField (SelectColumn (ColumnRef "users" "username")) (Just "username")
+                ]
+              , selectWhere =
+                  WhereCompare
+                    Equals
+                    (SelectColumn (ColumnRef "users" "id"))
+                    (SelectColumn (ColumnRef "posts" "user_id"))
+              }
+            ) (Just "username")
         ]
+    , selectWhere = WhereTrue
     }
 
 main :: IO ()
 main = do
-  forM_ (getQueryConstraints query) print
+  let aliases = getQueryAliases query
+  putStrLn "--- aliases ---"
+  forM_ (Map.toList aliases) $ \(name, val) -> do
+    print (name, val)
+  putStrLn "--- constraints ---"
+  forM_ (getQueryConstraints aliases query) print
+  putStrLn "--- result type ---"
+  print (getQueryResultType aliases query)
